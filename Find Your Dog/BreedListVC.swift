7f4a86aebe27ae8e67cdc2dcea818f3b0897d1e3
Aditya Vyavahare <vvaditya23@icloud.com>
2024-03-09 13:01:31 +0530

    tableview constraints and API fetching data.

--- Find Your Dog/BreedListVC.swift
+++ Find Your Dog/BreedListVC.swift
@@ -11,14 +11,16 @@ class BreedListVC: UIViewController {
 
     let breedListTableView = UITableView()
     
-    var breeds: [Breed] = []
+    var breedsArray: [Breed] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        fetchBreeds()
     }
 }
 
+//UI config
 extension BreedListVC {
     func setupUI() {
         view.addSubview(breedListTableView)
@@ -29,5 +31,43 @@ extension BreedListVC {
             breedListTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
             breedListTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
+    }
+}
+
+//fetch data
+extension BreedListVC {
+    func fetchBreeds() {
+        guard let url = URL(string: "https://dog.ceo/api/breeds/list/all") else {
+            print("Invalid URL")
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { [weak self] (data, response, error) in
+            guard let self = self else { return }
+            
+            if let error = error {
+                print("Error fetching breeds: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let data = data else {
+                print("No data received")
+                return
+            }
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                if let jsonData = json as? [String: Any],
+                   let breedsJSON = jsonData["message"] as? [String: Any] {
+                    self.breedsArray = breedsJSON.keys.map { Breed(name: $0) }
+                    DispatchQueue.main.async {
+                        // Update UI with fetched breeds
+                        print(self.breedsArray)
+                    }
+                }
+            } catch {
+                print("Error decoding JSON: \(error.localizedDescription)")
+            }
+        }.resume()
     }
 }
